19c630161ee70b54e1854dab10d384e77549fe2b
NaotoCoding <naoto.zaniah@gmail.com>
2024-05-28 23:22:05 +0900

    翻訳を連鎖する機構を構築

--- lambda_handler.rb
+++ lambda_handler.rb
@@ -4,27 +4,102 @@ require "json"
 
 class TranslationChain
   DEEPL_API_FREE_URI = "https://api-free.deepl.com/v2/translate".freeze
+  DEEPL_TRANSLATABLE_SOURCE_LANGS = {
+    "BG" => "ブルガリア語",
+    "CS" => "チェコ語",
+    "DA" => "デンマーク語",
+    "DE" => "ドイツ語",
+    "EL" => "ギリシャ語",
+    "EN" => "英語",
+    "ES" => "スペイン語",
+    "ET" => "エストニア語",
+    "FI" => "フィンランド語",
+    "FR" => "フランス語",
+    "HU" => "ハンガリー語",
+    "ID" => "インドネシア語",
+    "IT" => "イタリア語",
+    "JA" => "日本語",
+    "KO" => "韓国語",
+    "LT" => "リトアニア語",
+    "LV" => "ラトビア語",
+    "NB" => "ノルウェー語",
+    "NL" => "オランダ語",
+    "PL" => "ポーランド語",
+    "PT" => "ポルトガル語",
+    "RO" => "ルーマニア語",
+    "RU" => "ロシア語",
+    "SK" => "スロバキア語",
+    "SL" => "スロベニア語",
+    "SV" => "スウェーデン語",
+    "TR" => "トルコ語",
+    "UK" => "ウクライナ語",
+    "ZH" => "中国語"
+  }.freeze
+  DEEPL_TRANSLATABLE_TARGET_LANGS = {
+    "BG" => "ブルガリア語",
+    "CS" => "チェコ語",
+    "DA" => "デンマーク語",
+    "DE" => "ドイツ語",
+    "EL" => "ギリシャ語",
+    "EN-GB" => "英語（イギリス）",
+    "EN-US" => "英語（アメリカ）",
+    "ES" => "スペイン語",
+    "ET" => "エストニア語",
+    "FI" => "フィンランド語",
+    "FR" => "フランス語",
+    "HU" => "ハンガリー語",
+    "ID" => "インドネシア語",
+    "IT" => "イタリア語",
+    "JA" => "日本語",
+    "KO" => "韓国語",
+    "LT" => "リトアニア語",
+    "LV" => "ラトビア語",
+    "NB" => "ノルウェー語（ブークモール）",
+    "NL" => "オランダ語",
+    "PL" => "ポーランド語",
+    "PT-BR" => "ポルトガル語（ブラジル）",
+    "PT-PT" => "ポルトガル語（ポルトガル）",
+    "RO" => "ルーマニア語",
+    "RU" => "ロシア語",
+    "SK" => "スロバキア語",
+    "SL" => "スロベニア語",
+    "SV" => "スウェーデン語",
+    "TR" => "トルコ語",
+    "UK" => "ウクライナ語",
+    "ZH" => "中国語（簡体字・繁体字）"
+  }.freeze
 
   def initialize(translation_client_auth_key)
     @translation_client_auth_key = translation_client_auth_key
   end
 
-  def call
-    puts translate("JA", "DE", "こんにちは")
+  def call(initial_text:, initial_source_lang:, target_langs:)
+    check_arguments(initial_source_lang, target_langs)
+    initial_result = [lang: initial_source_lang, text: initial_text]
+    target_langs.each_with_object(initial_result).with_index do |(target_lang, result), index|
+      source_lang = index.zero? ? initial_source_lang : target_langs[index - 1]
+      text = index.zero? ? initial_text : result.last["text"]
+      result << { "lang" => target_lang, "text" => translate(text:, source_lang:, target_lang:) }
+    end
   end
 
   private
 
-    def translate(source_lang, target_lang, text)
+    def check_arguments(initial_source_lang, target_langs)
+      raise ArgumentError unless DEEPL_TRANSLATABLE_SOURCE_LANGS.keys.include?(initial_source_lang)
+      raise ArgumentError unless (target_langs - DEEPL_TRANSLATABLE_TARGET_LANGS.keys).empty?
+    end
+
+    def translate(text:, source_lang:, target_lang:)
       response = request_post(
         DEEPL_API_FREE_URI,
         {
           "Authorization" => "DeepL-Auth-Key #{@translation_client_auth_key}",
           "Content-Type" => "application/json"
         },
-        { source_lang:, target_lang:, text: [text] }
+        { text: [text], source_lang:, target_lang: }
       )
-      response.body
+      JSON.parse(response.body)["translations"].first["text"]
     end
 
     def request_post(end_point, headers, body, use_ssl: true)
@@ -32,9 +107,7 @@ class TranslationChain
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = use_ssl
       request = Net::HTTP::Post.new(uri)
-      headers.each do |key, value|
-        request[key] = value
-      end
+      headers.each { |key, value| request[key] = value }
       request.body = body.to_json
       https.request(request)
     end
@@ -43,3 +116,4 @@ end
 #--------------------------------------------------------------------------
 
 def lambda_handler; end
+
